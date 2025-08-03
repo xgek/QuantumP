e21bb1b5fb031cfa9d5809718f8845425bf11860
XGEK <73622401+xgek@users.noreply.github.com>
2025-08-03 21:44:35 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/pdfreader/main.go
@@ -0,0 +1,23 @@
+// cmd/pdfreader/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"pdfreader/internal/pdfreader"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := pdfreader.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
